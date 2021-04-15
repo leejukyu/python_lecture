b7df5edea11b058fc42e8eb9c1056bdc91fcf7e2
Name <dlwr777@gmail.com>
2021-04-15 15:32:12 +0900

    반복문 제어까지

---
+++ skip_vowel_break.py
@@ -0,0 +1,6 @@
+st = 'programming'
+for ch in st:
+    if ch in ['a','e','i','o','u']:
+        break
+    print(ch)
+print('The end')
---
+++ skip_vowel_continue.py
@@ -0,0 +1,6 @@
+st = 'programming'
+for ch in st:
+    if ch not in ['a','e','i','o','u']:
+        continue
+    print(ch)
+print('The end')
