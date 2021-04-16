aedcf223bfef32e31fa04a1dc0bda2e3a406d158
Name <dlwr777@gmail.com>
2021-04-16 16:18:16 +0900

    cat

--- object/init_cats.py
+++ object/init_cats.py
@@ -3,12 +3,13 @@ class Cat:
         self.name = name
         self.color = color
 
-    def meow(self):
+    def meow(self, name):
         print('내 이름은 {}, 색깔은 {} , 야옹'.format(self.name, self.color))
-        
+        print('속도는 ', name)
+
 nabi = Cat("나비")
-nabi.meow()
+nabi.meow(100)
 nero = Cat("네로","검은색")
-nero.meow()
+nero.meow(200)
 mimi = Cat("미미","갈색")
-mimi.meow()
+mimi.meow(300)
